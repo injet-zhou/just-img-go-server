7300db3bfc3e39ea026a99c18bb9dbf29cd6ef5e
injet-zhou <zhouyingjie@sinkcode.cn>
2022-06-02 20:50:01 +0800

    test: improve test coverage
    (cherry picked from commit 76c5eb611f915a0af2a08f9eee86bf8f6660d6c9)

--- tool/validate.go
+++ tool/validate.go
@@ -1,23 +1,38 @@
 package tool
 
-import "reflect"
+import (
+	"reflect"
+)
 
+// IsStructEmpty 判断struct是否为空
 func IsStructEmpty(s interface{}) bool {
 	v := reflect.ValueOf(s)
 	if v.Kind() == reflect.Ptr {
 		v = v.Elem()
 	}
+	if v.NumField() == 0 {
+		return true
+	}
+	isEmpty := false
 	for i := 0; i < v.NumField(); i++ {
 		f := v.Field(i)
 		if f.Kind() == reflect.Struct {
-			IsStructEmpty(f.Interface())
+			isEmpty = IsStructEmpty(f.Interface())
+			if isEmpty {
+				return isEmpty
+			}
 		} else if f.Kind() == reflect.Ptr {
 			if f.IsNil() {
 				return true
 			}
 			f = f.Elem()
-			if f.Kind() == reflect.Struct {
-				IsStructEmpty(f.Interface())
+			if f.Kind() == reflect.Struct || f.Kind() == reflect.Slice || f.Kind() == reflect.Map {
+				isEmpty = IsStructEmpty(f.Interface())
+				if isEmpty {
+					return isEmpty
+				}
+			} else if f.Len() == 0 || f.Int() == 0 || f.Float() == 0 {
+				return true
 			}
 		} else if f.Kind() == reflect.Slice {
 			if f.IsNil() {
@@ -28,8 +43,11 @@ func IsStructEmpty(s interface{}) bool {
 			}
 			for j := 0; j < f.Len(); j++ {
 				e := f.Index(j)
-				if e.Kind() == reflect.Struct {
-					IsStructEmpty(e.Interface())
+				if e.Kind() == reflect.Struct || e.Kind() == reflect.Slice || e.Kind() == reflect.Map {
+					isEmpty = IsStructEmpty(e.Interface())
+					if isEmpty {
+						return isEmpty
+					}
 				}
 			}
 		} else if f.Kind() == reflect.Map {
@@ -41,16 +59,29 @@ func IsStructEmpty(s interface{}) bool {
 			}
 			for _, k := range f.MapKeys() {
 				e := f.MapIndex(k)
-				if e.Kind() == reflect.Struct {
-					IsStructEmpty(e.Interface())
+				if e.Kind() == reflect.Struct || e.Kind() == reflect.Slice || e.Kind() == reflect.Map {
+					isEmpty = IsStructEmpty(e.Interface())
+					if isEmpty {
+						return isEmpty
+					}
+				} else if e.Len() == 0 || e.Int() == 0 || e.Float() == 0 {
+					return true
 				}
 			}
 		} else if f.Kind() == reflect.String {
 			if f.Len() == 0 {
 				return true
 			}
-		} else if f.Kind() == reflect.Int {
+		} else if f.Kind() == reflect.Int || f.Kind() == reflect.Int64 || f.Kind() == reflect.Int32 || f.Kind() == reflect.Int16 || f.Kind() == reflect.Int8 {
 			if f.Int() == 0 {
+				return true
+			}
+		} else if f.Kind() == reflect.Uint || f.Kind() == reflect.Uint64 || f.Kind() == reflect.Uint32 || f.Kind() == reflect.Uint16 || f.Kind() == reflect.Uint8 {
+			if f.Uint() == 0 {
+				return true
+			}
+		} else if f.Kind() == reflect.Float32 || f.Kind() == reflect.Float64 {
+			if f.Float() == 0 {
 				return true
 			}
 		}
--- tool/validate_test.go
+++ tool/validate_test.go
@@ -1,16 +1,191 @@
 package tool
 
 import (
-	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestIsStructEmpty(t *testing.T) {
-	type Foo struct {
-		Bar          string
-		IntField     int
-		BooleanField bool
+	// table test
+	type test struct {
+		name string
+		in   interface{}
+		out  bool
 	}
-	foo := new(Foo)
-	assert.Equal(t, true, IsStructEmpty(foo), "foo should be empty")
+	type Foo struct {
+	}
+	type Bar struct {
+		Name string
+	}
+	type Baz struct {
+		Int32Slice []int32
+	}
+	type Qux struct {
+		Foo Foo
+	}
+	type Quux struct {
+		Qux Qux
+	}
+	type Baza struct {
+		Bar Bar
+	}
+	str := ""
+	tests := []test{
+		test{
+			name: "empty struct",
+			in:   Foo{},
+			out:  true,
+		},
+		test{
+			name: "not empty struct",
+			in: Bar{
+				Name: "foo",
+			},
+			out: false,
+		},
+		test{
+			name: "empty struct pointer",
+			in:   new(Foo),
+			out:  true,
+		},
+		test{
+			name: "struct with empty struct",
+			in: Qux{
+				Foo: Foo{},
+			},
+			out: true,
+		},
+		test{
+			name: "empty single string field struct",
+			in: Bar{
+				Name: "",
+			},
+			out: true,
+		},
+		test{
+			name: "empty single int field struct",
+			in: struct {
+				Name int
+			}{
+				Name: 0,
+			},
+			out: true,
+		},
+		test{
+			name: "empty single float field struct",
+			in: struct {
+				Name float64
+			}{
+				Name: 0,
+			},
+			out: true,
+		},
+		test{
+			name: "empty single uint field struct",
+			in: struct {
+				Name uint
+			}{
+				Name: 0,
+			},
+			out: true,
+		},
+		test{
+			name: "empty single map field struct",
+			in: struct {
+				Name map[string]string
+			}{
+				Name: map[string]string{},
+			},
+			out: true,
+		},
+		test{
+			name: "empty single slice field struct",
+			in: &Baz{
+				Int32Slice: []int32{},
+			},
+			out: true,
+		},
+		test{
+			name: "empty struct with nil map",
+			in: struct {
+				Name map[string]string
+			}{
+				Name: nil,
+			},
+			out: true,
+		},
+		test{
+			name: "empty struct with nil slice",
+			in: struct {
+				Name []string
+			}{
+				Name: nil,
+			},
+			out: true,
+		},
+		test{
+			name: "struct with slice which it's elements is empty struct",
+			in: struct {
+				Name []Foo
+			}{
+				Name: []Foo{{}},
+			},
+			out: true,
+		},
+		test{
+			name: "struct embed struct which the embed struct is nil",
+			in: struct {
+				Name *Foo
+			}{
+				Name: nil,
+			},
+			out: true,
+		},
+		test{
+			name: "struct embed struct which the embed struct is empty",
+			in: struct {
+				Name *Quux
+			}{
+				Name: &Quux{
+					Qux: Qux{
+						Foo: Foo{},
+					},
+				},
+			},
+			out: true,
+		},
+		test{
+			name: "struct embed struct pointer which the embed struct is empty",
+			in: struct {
+				Name *string
+			}{
+				Name: &str,
+			},
+			out: true,
+		},
+		test{
+			name: "struct with map which it's elements is empty string",
+			in: struct {
+				Name map[string]string
+			}{
+				Name: map[string]string{"": ""},
+			},
+			out: true,
+		},
+		test{
+			name: "struct with map which it's elements is empty struct",
+			in: struct {
+				Name map[string]Foo
+			}{
+				Name: map[string]Foo{"foo": {}},
+			},
+			out: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := IsStructEmpty(tt.in); got != tt.out {
+				t.Errorf("IsStructEmpty() = %v, want %v", got, tt.out)
+			}
+		})
+	}
 }
